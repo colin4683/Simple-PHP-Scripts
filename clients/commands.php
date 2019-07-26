af7111e7f566a3f6f80b73daa236c8010b3d0dd8
Colin Stroble <38335987+colinstroble@users.noreply.github.com>
2019-07-25 20:25:32 -0400

    Update commands.php

--- clients/commands.php
+++ clients/commands.php
@@ -1,8 +1,8 @@
 <?php 
 $hostname = "localhost";
-$username = "pastterman";
-$pass = "JS9iHX4TdF";
-$con = mysqli_connect($hostname, $username, $pass, 'pastterman_phpdata');
+$username = "";
+$pass = "";
+$con = mysqli_connect($hostname, $username, $pass, '');
 
 
 
